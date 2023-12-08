fdd82741f5f50126073b7d21131daeb83db3bc6c
jose Franco <j.franco@tiendasdaka.com>
2023-12-08 09:05:24 -0400

    se subio la imagen del coco

--- routes/tc.js
+++ routes/tc.js
@@ -6,11 +6,17 @@ const router = express.Router();
 
 const multer  = require('multer');
 
-router.get('/',  async (req,res)=>{
+router.get('/4f64eaa44708eacdfb67703150ce5f05.jpg',  async (req,res)=>{
   
-    res.render('condicion')
+    res.redirect('/img//4f64eaa44708eacdfb67703150ce5f05.jpg')
   
      })
+     
+router.get('/',  async (req,res)=>{
+  
+  res.render('condicion')
+
+   })
 
 router.get('/:id',  async (req,res)=>{
   if(!req.params.id){
