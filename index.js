22a58a73bd9ac9d3c71144944dd39f1f4e2b35cd
Christianqr <romerochristian12@hotmail.com>
2024-05-28 17:54:24 -0400

    ajuste ruta mongo

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const router = express.Router();
 
  const mongoose = require('mongoose')
  //const mongouri = 'mongodb://software:gAB0n5P6&D@149.50.135.10:54210/dkstore'
- const mongouri = 'mongodb://sc:Y%3ByzC(Z9f%5ESc%5E%3C58Gx9Jwc0Y0lM5~o@3.23.208.239:27230/?authMechanism=DEFAULT'
+ const mongouri = 'mongodb://desarrollo:aHo12tThsG36z7HCk2g6OdLQ@3.23.208.239:27230/'
  //const mongouri = 'mongodb://localhost:27017/dkstore'
  
  // const mongouri = "mongodb+srv://jfranco:musiuito@cluster0.ogvcv9d.mongodb.net/?retryWrites=true&w=majority"
